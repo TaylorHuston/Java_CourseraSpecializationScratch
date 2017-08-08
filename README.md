ebb339af3919da5f64404350259da6db79820c9d
Taylor Huston <taylor.a.huston@gmail.com>
2017-08-07 23:48:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Scratch code for the first course in the Coursera Java Specialization
---
+++ SimpleLocation.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Taylor on 8/7/17.
+ */
+public class SimpleLocation {
+
+    public double latitude;
+    public double longitude;
+
+    public SimpleLocation(double lat, double lon) {
+        this.latitude = lat;
+        this.longitude = lon;
+    }
+    
+}
