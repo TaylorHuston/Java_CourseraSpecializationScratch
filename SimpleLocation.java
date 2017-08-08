67229e5789edf41d9ede049cf07a597b73db1e3f
Taylor Huston <taylor.a.huston@gmail.com>
2017-08-08 00:07:23 -0700

    Add setter and getters

--- SimpleLocation.java
+++ SimpleLocation.java
@@ -3,15 +3,41 @@
  */
 public class SimpleLocation {
 
-    public double latitude;
-    public double longitude;
+    private double latitude;
+    private double longitude;
 
     public SimpleLocation(double lat, double lon) {
         this.latitude = lat;
         this.longitude = lon;
     }
 
-    public double distance(SimpleLocation other) {
-
+    public double getLatitude() {
+        return this.latitude;
     }
+
+    public void setLatitude(double lat) {
+        if (lat < -90 || lat > 90) {
+            System.out.println("Illegal latitude value");
+        } else {
+            this.latitude = lat;
+        }
+    }
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    public void setLongitude(double lon) {
+        if (lon < -180 || lon > 18-) {
+            System.out.println("Illegal longitude value");
+        } else {
+            this.longitude = lon;
+        }
+    }
+
+    public double distance(SimpleLocation other) {
+        return getDist(this.latitude, this.longitude,
+                other.latitude, other.longitude);
+    }
+
 }
